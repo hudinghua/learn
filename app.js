5ca90bbb14ae53f9af2d2ba01143a9a611c605bc
huadinghu <huadinghu@qq.com>
2016-07-09 14:27:07 +0800

    第一次提交

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("app");
+console.log('document.body.clientWidth = ' + document.body.clientWidth);
+console.log('window.innerWidth = ' + window.innerWidth);
+console.log('screen.width = ' + screen.width);
